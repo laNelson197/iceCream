1e794a089a0820b7e48e8de1dcc6b338fcf40ff5
Lillian nelson <nelson.lillian@student.greenriver.edugit config --global user.email lanelson197@gmail.comgit config --global user.email nelson.lillian@student.greenriver.edu>
2024-01-09 13:39:08 -0500

    Added subtotal, tax, and total

--- process.php
+++ process.php
@@ -14,6 +14,14 @@
     <h1>Thank your for your order!</h1>
 
     <?php
+        //turn on error reporting
+        ini_set('display_errors', 1);
+        error_reporting(E_ALL);
+
+        define('PRICE_PER_SCOOP', 2.50);
+        define('SALES_TAX_RATE', 0.11);
+
+
         echo "<pre";
         var_dump($_POST);
         echo "</pre>";
@@ -22,10 +30,22 @@
         $flavors = $_POST['flavor'];
         $flavorString = implode(",", $flavors);
         $cone = $_POST['cone'];
+
+        //Calculate total due
+        $subtotal = PRICE_PER_SCOOP * $scoops;
+        $tax = $subtotal * SALES_TAX_RATE;
+        $total = $subtotal + $tax;
+
         //Print a summary
         echo "<p>$scoops scoops</p>";
         echo "<p>Flavors: $flavorString</p>";
         echo "<p>Cone: $cone</p>";
+
+        echo "<p>Subtotal: $subtotal</p>";
+        echo "<p>Tax: $tax</p>";
+        echo "<p>Total: $total</p>";
+
+
     ?>
 </body>
 </html>
